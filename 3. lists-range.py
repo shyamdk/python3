6dbb81e5e70e0eb35d1418ad78003dbabcea478f
Shyam Krishnamurthy <shyamdk@gmail.com>
2021-02-22 10:30:53 +0530

    new files

--- 3. lists-range.py
+++ 3. lists-range.py
@@ -2,8 +2,15 @@ x = [4.0, 5.0, 7.0]
 print(x)
 sumx = sum(x)
 countx = len(x)
+maxxx = max(x)
 print(sumx/countx)
+print('the max in the list is', maxxx)
 
+username = "Shyam"
+print(username.lower())
+
+student_grades = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+print(student_grades.count(10.0))
 y = list(range(0,10))
 z = list(range(1,10,2)) #every two items
 print(x) 
---
+++ 4. dict.py
@@ -0,0 +1,9 @@
+studentgrades = {"Mary": 8.1, "Sam": 8.8, "John": 7.5}
+print(studentgrades.values())
+print(studentgrades.keys())
+print(studentgrades.items())
+print(studentgrades.get("Mary"))
+print(studentgrades.pop("Mary"))
+print(studentgrades.items())
+day_temperatures = {'morning': 33.0, 'afternoon':38.0, 'evening': 32.0}
+print(day_temperatures)
