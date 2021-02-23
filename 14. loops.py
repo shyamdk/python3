56d5c16117c7e00efdab5df2b56fdac2f22976bd
Shyam Krishnamurthy <shyamdk@gmail.com>
2021-02-23 23:23:32 +0530

    2302

--- 14. loops.py
+++ 14. loops.py
@@ -15,6 +15,16 @@ for x in colors:
     if type(x) == int and x > 50:
         print(x)
 
-student_grades = { "mary: 30", "sam: 50", "ganesh; 60"}
-    for grades in student_grades:
-        
+student_grades = {"mary": 30, "sam": 50, "ganesh": 60}
+for grades in student_grades.items():
+    print(grades)
+
+phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
+ 
+for pair in phone_numbers.items():
+    print("{} has as phone number {}".format(pair[0], pair[1]))
+
+phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
+ 
+for key, value in phone_numbers.items():
+    print("{} has as phone number {}".format(key, value))
--- 4. dict.py
+++ 4. dict.py
@@ -9,3 +9,13 @@ day_temperatures = {'morning': 33.0, 'afternoon':38.0, 'evening': 32.0}
 print(day_temperatures)
 
 print(studentgrades["Sam"])
+
+phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
+ 
+for pair in phone_numbers.items():
+    print("{} has as phone number {}".format(pair[0], pair[1]))
+
+phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
+ 
+for key, value in phone_numbers.items():
+    print("{} has as phone number {}".format(key, value))
