9ec1ed30556257a009c64fca186fbc846a2c889f
Shyam Krishnamurthy <shyamdk@gmail.com>
2021-02-23 11:53:05 +0530

    2302

---
+++ 5. tuples.py
@@ -0,0 +1,4 @@
+#tuples are immutable, means you cannot append or remove elements from it
+monday_temperatures = (33.0, 55.0, 40.0)
+print(monday_temperatures)
+monday_temperatures.append("44.0")
---
+++ 6. Listoperations.py
@@ -0,0 +1,8 @@
+monday_temp = [9.1, 4.5, 2.0]
+monday_temp.append(7.0)
+print(monday_temp)
+monday_temp.remove(9.1)
+print(monday_temp)
+print(monday_temp.index(9.1))
+monday_temp.clear()
+print(monday_temp)
